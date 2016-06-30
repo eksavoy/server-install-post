4d1d6af390d835b7bb64f0adbf9cac85e04d3002
Romain de-coster <romain.decoster.rdc@gmail.com>
2016-06-30 12:02:56 +0200

    Update Fedora23.sh

--- Fedora23.sh
+++ Fedora23.sh
@@ -1,7 +1,10 @@
 /#!/usr/bin/env bash
 
-dnf update && dnf install policycoreutils policycoreutils-python checkpolicy git curl
+dnf update && dnf install policycoreutils policycoreutils-python checkpolicy git curl iptables-services && dnf remove docker
+systemctl disable firewalld && systemctl stop firewalld && systemctl unmask firewalld
+systemctl enable iptables.service && systemctl start iptables.service
 curl -fsSL https://get.docker.com/ | sh
+iptables -t filter -N DOCKER
 systemctl enable docker
 systemctl start docker
 groupadd docker
